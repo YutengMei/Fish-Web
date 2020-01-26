97e67cc7df354cda42ac959033d9db15905d2510
Yuteng Mei <yutengmei@gmail.com>
2020-01-25 21:54:13 -0500

    Added client side

---
+++ client/src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>Hello Bitches</div>;
+  }
+}
+
+export default App;
---
+++ config/dev.js
@@ -0,0 +1,10 @@
+// dev.js - don't commit this!!!
+// mongo password: h5E5dwSe17PkiX9b
+module.exports = {
+  googleClientID:
+    "73738329358-ejakgdppurp9n16btoamr8foaug3hgen.apps.googleusercontent.com",
+  mongoURI:
+    "mongodb+srv://myuteng:h5E5dwSe17PkiX9b@cluster0-iqof4.mongodb.net/test?retryWrites=true&w=majority",
+  googleClientSecret: "jSrzhEu32mpQV8HlGQZhOAiF",
+  cookieKey: "dsafnlsajfklsalf"
+};
