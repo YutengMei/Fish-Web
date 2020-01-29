676a7bbd14bd9b3eb73c2fe622635b8846d45d42
Yuteng Mei <yutengmei@gmail.com>
2020-01-28 22:06:33 -0500

    Added ui

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import * as actions from "../actions";
+import { connect } from "react-redux";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
   render() {
-    return <div>Hello Bitches</div>;
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
-export default App;
+export default connect(
+  null,
+  actions
+)(App);
---
+++ client/src/components/Header.js
@@ -0,0 +1,180 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import TimelineIcon from "@material-ui/icons/Timeline";
+import NotListedLocationIcon from "@material-ui/icons/NotListedLocation";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
+
+class Header extends Component {
+  state = { open: false };
+
+  toggleDrawer = open => event => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "shift")
+    ) {
+      return;
+    }
+
+    this.setState({ open: open });
+  };
+
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <a
+            href="/auth/google"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Login With Google
+          </a>
+        );
+      default:
+        return (
+          <a
+            href="/api/logout"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Logout
+          </a>
+        );
+    }
+  }
+
+  renderProfile() {
+    if (!this.props.auth) {
+      return (
+        <div>
+          <h1>"Motherfker"</h1>
+        </div>
+      );
+    }
+    const userName = this.props.auth.userName;
+    return (
+      <Grid
+        className={this.props.classes.profile}
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Avatar
+          alt={userName}
+          src={this.props.auth.profileImageUrl}
+          className={this.props.classes.large}
+        ></Avatar>
+        <h1>{userName}</h1>
+      </Grid>
+    );
+  }
+
+  renderMainList() {
+    return (
+      <div
+        className={this.props.classes.list}
+        role="presentation"
+        onClick={this.toggleDrawer(false)}
+        onKeyDown={this.toggleDrawer(false)}
+      >
+        {this.renderProfile()}
+        <Divider />
+        <List>
+          {["Tide Chart", "Find Spots"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <TimelineIcon /> : <NotListedLocationIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar position="absolute">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              className={this.props.classes.menuButton}
+              onClick={this.toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              className={this.props.classes.title}
+            >
+              Fish
+            </Typography>
+            <Button color="inherit">{this.renderContent()}</Button>
+          </Toolbar>
+        </AppBar>
+
+        <SwipeableDrawer
+          open={this.state.open}
+          onClose={this.toggleDrawer(false)}
+          onOpen={this.toggleDrawer(true)}
+        >
+          {this.renderMainList()}
+        </SwipeableDrawer>
+      </div>
+    );
+  }
+}
+
+const styles = theme => ({
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  list: {
+    width: 250
+  },
+  large: {
+    width: theme.spacing(15),
+    height: theme.spacing(15)
+  },
+  profile: {
+    marginTop: "50%",
+    marginBottom: "20%"
+  }
+});
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(Header));
