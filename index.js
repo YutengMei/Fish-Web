82a40b0571fa3f24c6cb105218e5087199c5b7d1
Yuteng Mei <yutengmei@gmail.com>
2020-01-28 22:16:11 -0500

    Updated

--- index.js
+++ index.js
@@ -34,6 +34,20 @@ app.use(passport.session());
 const authRoutes = require("./routes/authRoutes");
 authRoutes(app);
 
+// Handling different routing when the application is run in production
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Use heroku port or default localhost 5000
 const PORT = process.env.PORT || 5000;
 
