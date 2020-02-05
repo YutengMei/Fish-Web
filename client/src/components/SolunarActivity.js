59429bd873c2f8d84a399bffe4824e8c69ddfbec
Junchao Li <eggli@ucdavis.edu>
2020-02-04 22:40:43 -0500

    add two components Weather and SolunarActivity

---
+++ client/src/components/SolunarActivity.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class SolunarActivity extends React.Component {
+
+  render(){
+    return <div>Solunary</div>;
+  }
+}
+
+export default SolunarActivity;
---
+++ client/src/components/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class Weather extends React.Component {
+
+  render() {
+    return <div>weather</div>;
+  }
+}
+
+export default Weather;
